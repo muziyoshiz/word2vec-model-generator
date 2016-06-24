edc9a9aa8c2fa1569c230d254644003f28ee9161
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2016-06-24 23:57:01 +0900

    Modify methods to split

--- src/main/scala/jp/muziyoshiz/word2vec/Word2VecModelGenerator.scala
+++ src/main/scala/jp/muziyoshiz/word2vec/Word2VecModelGenerator.scala
@@ -4,19 +4,15 @@ import org.apache.spark._
 import org.apache.spark.rdd._
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
-import java.io.File
 
 object Word2VecModelGenerator {
 
   def main(args: Array[String]) {
-    require (args.length == 2, "argument: <textfilepath> <modelpath>")
+    require (args.length == 3, "argument: <textFilePath> <modelPath> <numPartition")
 
     val textFilePath = args(0)
     val modelPath = args(1)
-    if (new File(modelPath).exists) {
-      println("ERROR: Cannot overwrite existing model \"" + modelPath + "\"")
-      System.exit(1)
-    }
+    val numPartition = args(2).toInt
 
     val startTime = System.nanoTime
 
@@ -24,9 +20,12 @@ object Word2VecModelGenerator {
     val sc = new SparkContext(conf)
 
     try {
-      val input = sc.textFile(textFilePath).map(line => line.split(" ").toSeq)
+      val input = sc.textFile(textFilePath).repartition(sc.defaultParallelism * 3).map(line => line.split("""[ ,\."'\?!@\[\]{}:;()\|#=^~\\+*<>/_]+""").filter(_.length > 1).toSeq)
+
       val word2vec = new Word2Vec()
+      word2vec.setNumPartitions(numPartition)
       val model = word2vec.fit(input)
+
       model.save(sc, modelPath)
     } finally {
       sc.stop()
